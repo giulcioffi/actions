ed81a6ad7c6bc50e20ab882d6788fb0b56ddca09
giulcioffi <g.cioffi@arduino.cc>
2020-09-30 12:17:48 +0200

    Set global failure only if continue-on-error=False

--- libraries/compile-examples/compilesketches/compilesketches.py
+++ libraries/compile-examples/compilesketches/compilesketches.py
@@ -167,7 +167,8 @@ class CompileSketches:
         for sketch in sketch_list:
             compilation_result = self.compile_sketch(sketch_path=sketch)
             if not compilation_result.success:
-                all_compilations_successful = False
+                if self.continue_on_error is False:
+                    all_compilations_successful = False
 
             # Store the size data for this sketch
             sketches_report.append(self.get_sketch_report(compilation_result=compilation_result))
